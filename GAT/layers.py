421ac74791cd8ba8a5ee1799598c19bf96231a5c
Noman Ali <alinoman.itz@gmail.com>
2025-09-06 02:41:43 +0530

    GAT Layer code file from scratch

---
+++ GAT/layers.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+
+
+class GATLayer(torch.nn.Module):
+    
+    # We'll use these constants in many functions so just extracting them here as member fields
+    src_nodes_dim = 0  # position of source nodes in edge index
+    trg_nodes_dim = 1  # position of target nodes in edge index
+
+    nodes_dim = 0      
+    head_dim = 1       # attention head dim
+
+    def __init__(self, num_in_features, num_out_features, num_of_heads, concat=True, activation=nn.ELU(),
+                 dropout_prob=0.6, add_skip_connection=True, bias=True, log_attention_weights=False):
+
+        super().__init__()
+
+        self.num_of_heads = num_of_heads
+        self.num_out_features = num_out_features
+        self.concat = concat  # whether we should concatenate or average the attention heads
+        self.add_skip_connection = add_skip_connection
+        self.linear_proj = nn.Linear(num_in_features, num_of_heads * num_out_features, bias=False)
+        self.scoring_fn_target = nn.Parameter(torch.Tensor(1, num_of_heads, num_out_features))
+        self.scoring_fn_source = nn.Parameter(torch.Tensor(1, num_of_heads, num_out_features))
+
+        if bias and concat:
+            self.bias = nn.Parameter(torch.Tensor(num_of_heads * num_out_features))
+        elif bias and not concat:
+            self.bias = nn.Parameter(torch.Tensor(num_out_features))
+        else:
+            self.register_parameter('bias', None)
+
+        if add_skip_connection:
+            self.skip_proj = nn.Linear(num_in_features, num_of_heads * num_out_features, bias=False)
+        else:
+            self.register_parameter('skip_proj', None)
+
+        self.leakyReLU = nn.LeakyReLU(0.2)  # using 0.2 as in the paper, no need to expose every setting
+        self.activation = activation
+        self.dropout = nn.Dropout(p=dropout_prob)
+
+        self.log_attention_weights = log_attention_weights  # whether we should log the attention weights
+        self.attention_weights = None  # for later visualization purposes, I cache the weights here
+
+        self.init_params()
+        
+    def forward(self, data):
+
+        in_nodes_features, edge_index = data  # unpack data
+        num_of_nodes = in_nodes_features.shape[self.nodes_dim]
+        assert edge_index.shape[0] == 2, f'Expected edge index with shape=(2,E) got {edge_index.shape}'
+        in_nodes_features = self.dropout(in_nodes_features)
+
+        nodes_features_proj = self.linear_proj(in_nodes_features).view(-1, self.num_of_heads, self.num_out_features)
+
+        nodes_features_proj = self.dropout(nodes_features_proj)  # in the official GAT imp they did dropout here as well
+
+        scores_source = (nodes_features_proj * self.scoring_fn_source).sum(dim=-1)
+        scores_target = (nodes_features_proj * self.scoring_fn_target).sum(dim=-1)
+        scores_source_lifted, scores_target_lifted, nodes_features_proj_lifted = self.lift(scores_source, scores_target, nodes_features_proj, edge_index)
+        scores_per_edge = self.leakyReLU(scores_source_lifted + scores_target_lifted)
+
+        # shape = (E, NH, 1)
+        attentions_per_edge = self.neighborhood_aware_softmax(scores_per_edge, edge_index[self.trg_nodes_dim], num_of_nodes)
+        # Add stochasticity to neighborhood aggregation
+        attentions_per_edge = self.dropout(attentions_per_edge)
+        nodes_features_proj_lifted_weighted = nodes_features_proj_lifted * attentions_per_edge
+        out_nodes_features = self.aggregate_neighbors(nodes_features_proj_lifted_weighted, edge_index, in_nodes_features, num_of_nodes)
+
+        out_nodes_features = self.skip_concat_bias(attentions_per_edge, in_nodes_features, out_nodes_features)
+        return (out_nodes_features, edge_index)
+        
+    def neighborhood_aware_softmax(self, scores_per_edge, trg_index, num_of_nodes):
+        # Calculate the numerator. Make logits <= 0 so that e^logit <= 1 (this will improve the numerical stability)
+        scores_per_edge = scores_per_edge - scores_per_edge.max()
+        exp_scores_per_edge = scores_per_edge.exp()  # softmax
+
+        # Calculate the denominator. shape = (E, NH)
+        neigborhood_aware_denominator = self.sum_edge_scores_neighborhood_aware(exp_scores_per_edge, trg_index, num_of_nodes)
+
+        attentions_per_edge = exp_scores_per_edge / (neigborhood_aware_denominator + 1e-16)
+
+        # shape = (E, NH) -> (E, NH, 1) so that we can do element-wise multiplication with projected node features
+        return attentions_per_edge.unsqueeze(-1)
+
+    def sum_edge_scores_neighborhood_aware(self, exp_scores_per_edge, trg_index, num_of_nodes):
+
+        trg_index_broadcasted = self.explicit_broadcast(trg_index, exp_scores_per_edge)
+
+        size = list(exp_scores_per_edge.shape)  # convert to list otherwise assignment is not possible
+        size[self.nodes_dim] = num_of_nodes
+        neighborhood_sums = torch.zeros(size, dtype=exp_scores_per_edge.dtype, device=exp_scores_per_edge.device)
+
+        neighborhood_sums.scatter_add_(self.nodes_dim, trg_index_broadcasted, exp_scores_per_edge)
+
+        return neighborhood_sums.index_select(self.nodes_dim, trg_index)
+
+    def aggregate_neighbors(self, nodes_features_proj_lifted_weighted, edge_index, in_nodes_features, num_of_nodes):
+        size = list(nodes_features_proj_lifted_weighted.shape)  # convert to list otherwise assignment is not possible
+        size[self.nodes_dim] = num_of_nodes  # shape = (N, NH, FOUT)
+        out_nodes_features = torch.zeros(size, dtype=in_nodes_features.dtype, device=in_nodes_features.device)
+
+        # shape = (E) -> (E, NH, FOUT)
+        trg_index_broadcasted = self.explicit_broadcast(edge_index[self.trg_nodes_dim], nodes_features_proj_lifted_weighted)
+        out_nodes_features.scatter_add_(self.nodes_dim, trg_index_broadcasted, nodes_features_proj_lifted_weighted)
+
+        return out_nodes_features
+
+    def lift(self, scores_source, scores_target, nodes_features_matrix_proj, edge_index):
+        src_nodes_index = edge_index[self.src_nodes_dim]
+        trg_nodes_index = edge_index[self.trg_nodes_dim]
+
+        # Using index_select is faster than "normal" indexing (scores_source[src_nodes_index]) in PyTorch!
+        scores_source = scores_source.index_select(self.nodes_dim, src_nodes_index)
+        scores_target = scores_target.index_select(self.nodes_dim, trg_nodes_index)
+        nodes_features_matrix_proj_lifted = nodes_features_matrix_proj.index_select(self.nodes_dim, src_nodes_index)
+
+        return scores_source, scores_target, nodes_features_matrix_proj_lifted
+
+    def explicit_broadcast(self, this, other):
+        # Append singleton dimensions until this.dim() == other.dim()
+        for _ in range(this.dim(), other.dim()):
+            this = this.unsqueeze(-1)
+
+        # Explicitly expand so that shapes are the same
+        return this.expand_as(other)
+
+    def init_params(self):
+        nn.init.xavier_uniform_(self.linear_proj.weight)
+        nn.init.xavier_uniform_(self.scoring_fn_target)
+        nn.init.xavier_uniform_(self.scoring_fn_source)
+
+        if self.bias is not None:
+            torch.nn.init.zeros_(self.bias)
+
+    def skip_concat_bias(self, attention_coefficients, in_nodes_features, out_nodes_features):
+        if self.log_attention_weights:  # potentially log for later visualization in playground.py
+            self.attention_weights = attention_coefficients
+
+        if self.add_skip_connection:  # add skip or residual connection
+            if out_nodes_features.shape[-1] == in_nodes_features.shape[-1]:  # if FIN == FOUT
+
+                out_nodes_features += in_nodes_features.unsqueeze(1)
+            else:
+
+                out_nodes_features += self.skip_proj(in_nodes_features).view(-1, self.num_of_heads, self.num_out_features)
+
+        if self.concat:
+            # shape = (N, NH, FOUT) -> (N, NH*FOUT)
+            out_nodes_features = out_nodes_features.view(-1, self.num_of_heads * self.num_out_features)
+        else:
+            # shape = (N, NH, FOUT) -> (N, FOUT)
+            out_nodes_features = out_nodes_features.mean(dim=self.head_dim)
+
+        if self.bias is not None:
+            out_nodes_features += self.bias
+
+        return out_nodes_features if self.activation is None else self.activation(out_nodes_features)
