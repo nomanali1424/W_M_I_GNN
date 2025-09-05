125fd0c547b185a2764845ae36c9fa2f462ba3e3
Noman Ali <alinoman.itz@gmail.com>
2025-09-06 02:41:58 +0530

    GAT model file from scratch

---
+++ GAT/models.py
@@ -0,0 +1,36 @@
+import torch.nn as nn
+from torch.optim import Adam
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+from layers import GATLayer
+
+class GAT(nn.Module):
+
+    def __init__(self, num_of_layers, num_heads_per_layer, num_features_per_layer, add_skip_connection=True, bias=True,
+                 dropout=0.6, log_attention_weights=False):
+        super().__init__()
+        assert num_of_layers == len(num_heads_per_layer) == len(num_features_per_layer) - 1, f'Enter valid arch params.'
+
+        num_heads_per_layer = [1] + num_heads_per_layer  # trick - so that I can nicely create GAT layers below
+
+        gat_layers = []  # collect GAT layers
+        for i in range(num_of_layers):
+            layer = GATLayer(
+                num_in_features=num_features_per_layer[i] * num_heads_per_layer[i],  # consequence of concatenation
+                num_out_features=num_features_per_layer[i+1],
+                num_of_heads=num_heads_per_layer[i+1],
+                concat=True if i < num_of_layers - 1 else False,  # last GAT layer does mean avg, the others do concat
+                activation=nn.ELU() if i < num_of_layers - 1 else None,  # last layer just outputs raw scores
+                dropout_prob=dropout,
+                add_skip_connection=add_skip_connection,
+                bias=bias,
+                log_attention_weights=log_attention_weights
+            )
+            gat_layers.append(layer)
+
+        self.gat_net = nn.Sequential(
+            *gat_layers,
+        )
+
+    def forward(self, data):
+        return self.gat_net(data)
